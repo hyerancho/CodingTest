5fb02e97eb1e1742d0360ee11d7a40f8fc7ef2ac
hyerancho <129468803+hyerancho@users.noreply.github.com>
2024-10-27 22:49:50 +0900

    [level 0] Title: 문자열 돌리기, Time: 222.04 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+         int num = a.length();
+        
+        for(int i = 0; i < num; i++) {
+            
+            System.out.println(a.charAt(i));
+            
+        }
+    }
+}
